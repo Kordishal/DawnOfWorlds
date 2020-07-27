a6d22b9fee0948cf9c79103a6d94a19c05d78e90
Jonas Waeber <jonas.waeber@unibas.ch>
2020-07-27 14:35:43 +0200

    Implements deity factory with a concurrent dictionary!

--- Assets/Scripts/Player/Data/DeityFactory.cs
+++ Assets/Scripts/Player/Data/DeityFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,9 +27,10 @@ namespace Player.Data
         private int NextDeityIdentifier => _currentMaxIdentifier + 1;
 
         private readonly string _saveFile;
-        private readonly DeityCollection _collection;
+        private readonly ConcurrentDictionary<int, Deity> _deities;
 
         private Deity _currentDeity;
+
         public Deity CurrentDeity
         {
             get => _currentDeity;
@@ -39,18 +41,21 @@ namespace Player.Data
                 _currentDeity = value;
             }
         }
+
         public event EventHandler<ChangedCurrentDeityEventUpdate> OnCurrentDeityChange;
 
         private void OnCurrentDeityChanged([CanBeNull] Deity deity)
         {
             OnCurrentDeityChange?.Invoke(this, new ChangedCurrentDeityEventUpdate(deity));
         }
+
         public event EventHandler<EventArgs> OnDeityListChange;
 
         private void OnDeityListChanged()
         {
             OnDeityListChange?.Invoke(this, new EventArgs());
         }
+
         private DeityFactory(string basePath)
         {
             var directory = Path.Combine(basePath, DirectoryName);
@@ -59,7 +64,9 @@ namespace Player.Data
                 Directory.CreateDirectory(directory);
             if (!File.Exists(_saveFile))
                 File.Create(_saveFile).Close();
-            _collection = JsonUtility.FromJson<DeityCollection>(File.ReadAllText(_saveFile)) ?? new DeityCollection();
+            var deityCollection = JsonUtility.FromJson<DeityCollection>(File.ReadAllText(_saveFile)) ??
+                                  new DeityCollection();
+            _deities = deityCollection.ToConcurrentDictionary();
             foreach (var deity in GetDeities())
             {
                 if (deity.identifier > _currentMaxIdentifier)
@@ -72,43 +79,48 @@ namespace Player.Data
         public void CreateDeity(string name)
         {
             var deity = new Deity(NextDeityIdentifier, name);
-            if (_collection.deities == null) _collection.deities = new List<Deity>();
-            _collection.deities.Add(deity);
-            _currentMaxIdentifier += 1;
-            CurrentDeity = deity;
-            SaveDeities();
+            if (_deities.TryAdd(deity.identifier, deity))
+            {
+                _currentMaxIdentifier += 1;
+                CurrentDeity = deity;
+                SaveDeities();
+            }
         }
 
         public bool DeleteDeity(int identifier)
         {
-            if (identifier >= NextDeityIdentifier) return false;
-            if (!_collection.deities.Remove(new Deity(identifier))) return false;
+            if (!_deities.TryRemove(identifier, out _)) return false;
             CurrentDeity = null;
             SaveDeities();
             return true;
         }
 
         public void UpdateDeity(Deity deity)
-        {
-            _collection.deities.Remove(deity);
-            _collection.deities.Add(deity);
-            CurrentDeity = deity;
-            SaveDeities();
+        { 
+            if (_deities.TryUpdate(deity.identifier, deity, _deities[deity.identifier]))
+            {
+                CurrentDeity = deity;
+                SaveDeities();
+            }
         }
-
-        [CanBeNull]
-        public Deity GetDeity(int identifier) => identifier >= NextDeityIdentifier
-            ? null
-            : _collection.deities.Find(deity => deity.identifier == identifier);
+        public Deity GetDeity(int identifier)
+        {
+            return _deities[identifier];
+        }
 
         public IEnumerable<Deity> GetDeities()
         {
-            return _collection?.deities ?? Enumerable.Empty<Deity>();
+            return _deities.Values;
         }
-        
+
         private void SaveDeities()
         {
-            File.WriteAllText(_saveFile, JsonUtility.ToJson(_collection.deities.OrderBy(deity => deity.identifier)));
+            var collection = new DeityCollection
+            {
+                deities = _deities.Values.ToList()
+            };
+
+            File.WriteAllText(_saveFile, JsonUtility.ToJson(collection));
             OnDeityListChanged();
         }
 
@@ -116,6 +128,11 @@ namespace Player.Data
         private class DeityCollection
         {
             public List<Deity> deities;
+
+            public ConcurrentDictionary<int, Deity> ToConcurrentDictionary()
+            {
+                return new ConcurrentDictionary<int, Deity>(deities.Cast<KeyValuePair<int, Deity>>());
+            }
         }
     }
 }
